38fcef32e46577b3cfd2d04c02ae85ea5d0e2610
Walter Werner <wernwa@gmail.com>
2014-06-17 10:22:57 -0700

    gui functions calling from threads over events

--- gui/TabStripChart.py
+++ gui/TabStripChart.py
@@ -8,6 +8,7 @@
 import io
 import os
 import wx
+import wx.lib.newevent
 from epics import PV
 from Experiment import *
 from PowerSupplyControls import PowerSupplyControls
@@ -21,14 +22,17 @@ import time
 class StripChartMemory:
     time = []
     data = []
-    limit = None
+
 
     def __init__(self, limit=60):
         self.limit = limit
 
     def add(self, t,d):
-        self.time.insert(0,t)
-        self.data.insert(0,d)
+        self.time.append(t)
+        self.data.append(d)
+
+        #self.time.insert(0,t)
+        #self.data.insert(0,d)
 
     def delete_data_over_limit(self):
         time_now = time.time()
@@ -41,6 +45,8 @@ class StripChartMemory:
 
 
 class TabStripChart(wx.Panel):
+
+    use_pipes_for_gnuplot = True
 
     st_quad1 = None
     b_show = None
@@ -65,20 +71,16 @@ class TabStripChart(wx.Panel):
 
 
         self.b_show = wx.Button(parent=panel, pos=wx.Point(50, 490), label="Start StripChart")
-        #self.b_show.Bind(wx.EVT_BUTTON, self.OnStartStop_StripChart)
-        #self.b_show.Bind(wx.EVT_BUTTON, self.UpdateStripChart)
-        self.b_show.Bind(wx.EVT_BUTTON, self.UpdateStripChart2)
+        self.b_show.Bind(wx.EVT_BUTTON, self.OnStartStop_StripChart)
         panel.SetSizer(hbox)
 
         # init the pipes for gnuplot
 
-        #os.system('mkfifo %s' %(self.chart_data_pipe))
-        #os.system('mkfifo %s' %(self.chart_image_pipe))
-        #os.system('mkfifo %s' %(self.chart_gp_pipe))
+        if self.use_pipes_for_gnuplot:
+            os.system('FILE=%s; if [ ! -f $FILE ]; then mkfifo $FILE; fi' %(self.chart_data_pipe))
+            os.system('FILE=%s; if [ ! -f $FILE ]; then mkfifo $FILE; fi' %(self.chart_image_pipe))
+            os.system('FILE=%s; if [ ! -f $FILE ]; then mkfifo $FILE; fi' %(self.chart_gp_pipe))
 
-        #os.system('if [ ! -f %s ]; then mkfifo %s; fi' %(self.chart_data_pipe,self.chart_data_pipe))
-        #os.system('if [ ! -f %s ]; then mkfifo %s; fi' %(self.chart_image_pipe,self.chart_image_pipe))
-        #os.system('if [ ! -f %s ]; then mkfifo %s; fi' %(self.chart_gp_pipe,self.chart_gp_pipe))
 
         def fill_random_data():
             while True:
@@ -86,21 +88,20 @@ class TabStripChart(wx.Panel):
                 time.sleep(1+random())
         start_new_thread( fill_random_data ,() )
 
-        def repeat_charts_forever():
-            while True:
-                time.sleep(1)
-                self.UpdateStripChart2()
-        start_new_thread( repeat_charts_forever,() )
+
+
+
 
     # StripChart start stop
     def OnStartStop_StripChart(self, event):
         self.strip_chart_continue = not(self.strip_chart_continue)
         def StripChartLoop():
             while self.strip_chart_continue:
-                print 'inside StripChartLoop'
-                #self.UpdateStripChart(event)
-                self.UpdateStripChart2(event)
-                time.sleep(1)
+                if self.use_pipes_for_gnuplot:
+                    self.UpdateStripChart(event)
+                else:
+                    self.UpdateStripChart2(event)
+                time.sleep(0.5)
         if self.strip_chart_continue == True:
             self.b_show.SetLabel('stop')
             start_new_thread( StripChartLoop,() )
@@ -119,6 +120,7 @@ class TabStripChart(wx.Panel):
         def writeGPlotData():
             self.strip_random.delete_data_over_limit()
             data = '# t random\n'
+            print 'data count %d'%len(self.strip_random.time)
             for i in range(len(self.strip_random.time)):
                 time_now = time.time()
                 data += '%0.0f %0.3f\n' %(self.strip_random.time[i]-time_now,self.strip_random.data[i])
@@ -142,29 +144,32 @@ class TabStripChart(wx.Panel):
             gp_pipe.close()
 
 
-        ### read image data produced by gnuplot ###
-        with io.open(self.chart_image_pipe, 'rb') as pipe_pipe:
-            buff = pipe_pipe.read()
-            stream = io.BytesIO(buff)
-            image = wx.ImageFromStream(stream).ConvertToBitmap()
-            pipe_pipe.close()
+        ## gui changes call from main process
+        def last_part_for_event_gui(evt):
+            ### read image data produced by gnuplot ###
+            with io.open(self.chart_image_pipe, 'rb') as pipe_pipe:
+                buff = pipe_pipe.read()
+                stream = io.BytesIO(buff)
+                image = wx.ImageFromStream(stream).ConvertToBitmap()
+                pipe_pipe.close()
 
 
-        if self.imagewx == None:
-            self.imagewx = wx.StaticBitmap(self, -1, image, (5, 5), (image.GetWidth(), image.GetHeight()))
-        else:
-            self.imagewx.SetBitmap(image)
+            if self.imagewx == None:
+                self.imagewx = wx.StaticBitmap(self, -1, image, (5, 5), (image.GetWidth(), image.GetHeight()))
+            else:
+                self.imagewx.SetBitmap(image)
 
-        diff_time = time.time() - start_time
+            diff_time = time.time() - start_time
 
-        print '%0.3fs' %(diff_time)
+            print '%0.3fs' %(diff_time)
+        self.call_routine_over_event( last_part_for_event_gui )
 
 
     def __del__(self):
         ### delete temporary gnuplot files ###
-        os.system('FILE=%s; if [ -f $FILE ]; then rm $FILE; fi' %(self.chart_data_pipe))
-        os.system('FILE=%s; if [ -f $FILE ]; then rm $FILE; fi' %(self.chart_image_pipe))
-        os.system('FILE=%s; if [ -f $FILE ]; then rm $FILE; fi' %(self.chart_gp_pipe))
+        os.system('FILE=%s; if [ -e $FILE ]; then rm $FILE; fi' %(self.chart_data_pipe))
+        os.system('FILE=%s; if [ -e $FILE ]; then rm $FILE; fi' %(self.chart_image_pipe))
+        os.system('FILE=%s; if [ -e $FILE ]; then rm $FILE; fi' %(self.chart_gp_pipe))
 
 
     # UpdateStripChart without pipes
@@ -185,23 +190,35 @@ class TabStripChart(wx.Panel):
                 f.close()
         writeGPlotData()
 
-#        data = '# t random\n'
-#        for i in range(1,100):
-#            data += '%i %0.3f\n' %(i,random())
-#            with io.open(self.chart_data_pipe, 'w') as f:
-#                f.write(unicode(data))
-#                f.close()
         os.system('gnuplot %s' %self.chart_gp_file)
 
 
-        image = wx.Image(self.chart_image_pipe).ConvertToBitmap()
+        ## gui changes call from main process
+        def last_part_for_event_gui(evt):
+            image = wx.Image(self.chart_image_pipe).ConvertToBitmap()
 
-        if self.imagewx == None:
-            self.imagewx = wx.StaticBitmap(self, -1, image, (5, 5), (image.GetWidth(), image.GetHeight()))
-        else:
-            self.imagewx.SetBitmap(image)
+            if self.imagewx == None:
+                self.imagewx = wx.StaticBitmap(self, -1, image, (5, 5), (image.GetWidth(), image.GetHeight()))
+            else:
+                self.imagewx.SetBitmap(image)
 
-        diff_time = time.time() - start_time
+            diff_time = time.time() - start_time
 
-        print '%0.3fs' %(diff_time)
+            print '%0.3fs' %(diff_time)
 
+
+        self.call_routine_over_event( last_part_for_event_gui )
+
+
+    SomeNewEvent=None
+    def call_routine_over_event(self, handler):
+
+        if self.SomeNewEvent==None:
+            self.SomeNewEvent, self.EVT_SOME_NEW_EVENT = wx.lib.newevent.NewEvent()
+            self.Bind(self.EVT_SOME_NEW_EVENT, handler)
+
+            # Create the event
+            self.evt = self.SomeNewEvent()
+
+        # Post the event
+        wx.PostEvent(self, self.evt)
