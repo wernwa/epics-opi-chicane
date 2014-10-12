69d04b9594930eb43043fd689eb3f52c854d8d6e
Walter Werner <wernwa@gmail.com>
2014-10-12 11:05:00 -0700

    test file for loading

---
+++ set-current-magnets.py
@@ -0,0 +1,25 @@
+
+
+
+print 'Setting the magnets to pi'
+
+mquad1.ps.Curr.put(3.14)
+print 'q1'
+mquad2.ps.Curr.put(3.14)
+print 'q2'
+mquad3.ps.Curr.put(3.14)
+print 'q3'
+mquad4.ps.Curr.put(3.14)
+print 'q4'
+mquad5.ps.Curr.put(3.14)
+print 'q5'
+mquad6.ps.Curr.put(3.14)
+print 'q6'
+mquad7.ps.Curr.put(3.14)
+print 'q7'
+mdipol1.ps.Curr.put(3.14)
+print 'd1'
+mdipol2.ps.Curr.put(3.14)
+print 'd2'
+
+print 'done'
